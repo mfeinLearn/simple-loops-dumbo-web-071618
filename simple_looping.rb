ae1b878329afc8f081974d447805bc73806b9cb3
Malcome Fein <mfein90@gmail.com>
2018-07-15 09:52:04 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -8,27 +8,52 @@ def loop_iterator(number_of_times)
   #   correct number of times?
   # Maybe we should keep count of the number of times we've puts out the 
   #  phrase and break when the counter hits the appropriate number...
-  phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  # phrase = "Welcome to Flatiron School's Web Development Course!"
+  
+  counter = 0
+  loop do
+    puts "Welcome to Flatiron School's Web Development Course!"
+    counter = counter + 1
+    break if counter == number_of_times
+  end
 end
+
+
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  # phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  7.times do 
+  puts "Welcome to Flatiron School's Web Development Course!"
+  end
 end
 
 def while_iterator(number_of_times)
   # code your solution here using the "while" keyword
   # hint: user a counter to tell the while loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  # phrase = "Welcome to Flatiron School's Web Development Course!"
+  
+  counter = 0
+
+  while counter < number_of_times
+    counter += 1
+    puts "Welcome to Flatiron School's Web Development Course!"
+  end
   
 end
 
 def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  # phrase = "Welcome to Flatiron School's Web Development Course!"
+  
+  counter = 0
+  until counter == 7
+    puts "Welcome to Flatiron School's Web Development Course!"
+    counter += 1
+  end
 
 end
 
@@ -36,7 +61,13 @@ def for_iterator(number_of_times)
   # code your solution here using the "for" keyword
   # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+  # phrase = "Welcome to Flatiron School's Web Development Course!"
+  
+  count = 1..7
+ 
+  for phrase in count 
+      puts "Welcome to Flatiron School's Web Development Course!"
+  end
   
 end
 
